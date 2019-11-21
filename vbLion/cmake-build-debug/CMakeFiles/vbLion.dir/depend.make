f528db25f495223815c9949c8586d98ede68aa7a
Samuel Kratsas <skratsas17@mail.bw.edu>
2019-11-21 15:41:44 -0500

    sk

---
+++ vbLion/cmake-build-debug/CMakeFiles/vbLion.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for vbLion.
+# This may be replaced when dependencies are built.
---
+++ vbLion/library.h
@@ -0,0 +1,6 @@
+#ifndef VBLION_LIBRARY_H
+#define VBLION_LIBRARY_H
+
+void hello(void);
+
+#endif //VBLION_LIBRARY_H
